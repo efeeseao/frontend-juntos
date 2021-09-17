1e9e5e31dd592baa12c5a0e1b61fee0592f5f409
Fernando Santos <nando4803@gmail.com>
2021-09-17 13:52:26 +0100

    feat: add social media

---
+++ src/resources/socialMedia.js
@@ -0,0 +1,24 @@
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from 'react-icons/fa'
+
+const SocialData = [
+  {
+    icon: <FaFacebookF style={{ color: '#3b5998' }} />,
+    url: 'https://facebook.com/juntossomosmais',
+    target: '_blank',
+    aria: 'Facebook'
+  },
+  {
+    icon: <FaInstagram style={{ color: '#DD2A7B' }} />,
+    url: 'https://instagram.com/juntossomosmais',
+    target: '_blank',
+    aria: 'Instagram'
+  },
+  {
+    icon: <FaLinkedinIn style={{ color: '#0077B5' }} />,
+    url: 'https://linkedin.com/in/juntossomosmais',
+    target: '_blank',
+    aria: 'Linkedin'
+  }
+]
+
+export default SocialData
