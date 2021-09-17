3b3e798d054181d5926251c756d89bad59e3f29c
Fernando Santos <nando4803@gmail.com>
2021-09-17 13:07:14 +0100

    feat: add navbar data

---
+++ src/resources/navbarData.js
@@ -0,0 +1,8 @@
+import { v4 } from 'uuid'
+
+const NavbarData = [
+  { name: 'Home', route: '/', id: v4() },
+  { name: 'Lista de Membros', route: '/members', id: v4() }
+]
+
+export default NavbarData
