dfc768a29b87112cca142f233417846b7404e52b
Fernando Santos <nando4803@gmail.com>
2021-09-19 23:21:39 +0100

    feat: add sortedby and states list

---
+++ src/components/SortedByState/index.jsx
@@ -0,0 +1,158 @@
+import { useState, useEffect } from 'react'
+
+import range from 'components/Shared/rage'
+import leftArrow from 'images/left-path.png'
+import rightArrow from 'images/right-path.png'
+
+import { PaginationWrapper } from 'components/Pagination/pagination.styles'
+
+const LEFT_PAGE = 'LEFT'
+const RIGHT_PAGE = 'RIGHT'
+
+const SortedByState = (props) => {
+  const [isCurrentPage, setIsCurrentPage] = useState(1)
+  let { totalMembers, pageLimit, pageNeighbors } = props
+
+  const totalPages = Math.ceil(totalMembers / pageLimit)
+
+  const gotoPage = (page) => {
+    const { onPageChanged = (i) => i } = props
+
+    const currentPage = Math.max(0, Math.min(page, totalPages))
+
+    const paginationData = {
+      currentPage,
+      totalPages,
+      pageLimit,
+      totalMembers
+    }
+    setIsCurrentPage(currentPage)
+    onPageChanged(paginationData)
+  }
+
+  useEffect(() => {
+    gotoPage(1)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleClick = (page, evt) => {
+    evt.preventDefault()
+    gotoPage(page)
+  }
+
+  const handleMoveLeft = (evt) => {
+    evt.preventDefault()
+    gotoPage(isCurrentPage - pageNeighbors * 2 - 1)
+  }
+
+  const handleMoveRight = (evt) => {
+    evt.preventDefault()
+    gotoPage(isCurrentPage + pageNeighbors * 2 + 1)
+  }
+
+  const fetchPageNumbers = () => {
+    const totalNumbers = pageNeighbors * 2 + 3
+    const totalBlocks = totalNumbers + 2
+
+    if (totalPages > totalBlocks) {
+      let pages = []
+
+      const leftBound = isCurrentPage - pageNeighbors
+      const rightBound = isCurrentPage + pageNeighbors
+      const beforeLastPage = totalPages - 1
+
+      const startPage = leftBound > 2 ? leftBound : 2
+      const endPage = rightBound < beforeLastPage ? rightBound : beforeLastPage
+
+      pages = range(startPage, endPage)
+
+      const pagesCount = pages.length
+      const singleSpillOffset = totalNumbers - pagesCount - 1
+
+      const leftSpill = startPage > 2
+      const rightSpill = endPage < beforeLastPage
+
+      const leftSpillPage = LEFT_PAGE
+      const rightSpillPage = RIGHT_PAGE
+
+      if (leftSpill && !rightSpill) {
+        const extraPages = range(startPage - singleSpillOffset, startPage - 1)
+        pages = [leftSpillPage, ...extraPages, ...pages]
+      } else if (!leftSpill && rightSpill) {
+        const extraPages = range(endPage + 1, endPage + singleSpillOffset)
+        pages = [...pages, ...extraPages, rightSpillPage]
+      } else if (leftSpill && rightSpill) {
+        pages = [leftSpillPage, ...pages, rightSpillPage]
+      }
+
+      return [1, ...pages, totalPages]
+    }
+
+    return range(1, totalPages)
+  }
+
+  if (!totalMembers) return null
+
+  if (totalPages === 1) return null
+
+  const pages = fetchPageNumbers()
+
+  return (
+    <PaginationWrapper>
+      <button
+        type="button"
+        className="left-button"
+        disabled={isCurrentPage === 1}
+        onClick={(event) => handleClick(isCurrentPage - 1, event)}
+      >
+        <img src={leftArrow} alt="button-icon" />
+      </button>
+      <ul className="pagination">
+        {pages.map((page, index) => {
+          if (page === LEFT_PAGE)
+            return (
+              <li key={index} className="page-item">
+                <a className="page-link" href="#" onClick={handleMoveLeft}>
+                  <span>&laquo;</span>
+                </a>
+              </li>
+            )
+
+          if (page === RIGHT_PAGE)
+            return (
+              <li key={index} className="page-item">
+                <a className="page-link" href="#" onClick={handleMoveRight}>
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            )
+
+          return (
+            <li
+              key={index}
+              className={`page-item${isCurrentPage === page ? ' active' : ''}`}
+            >
+              <a
+                className="page-link"
+                href="#"
+                onClick={(event) => handleClick(page, event)}
+              >
+                {page}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+      <button
+        type="button"
+        className="right-button"
+        disabled={isCurrentPage === 23}
+        onClick={(event) => handleClick(isCurrentPage + 1, event)}
+      >
+        <img src={rightArrow} alt="button-icon" />
+      </button>
+    </PaginationWrapper>
+  )
+}
+
+export default SortedByState
--- src/components/States/index.jsx
+++ src/components/States/index.jsx
@@ -1,23 +1,53 @@
+import { useState, useEffect, useContext } from 'react'
+import { CheckedStatesContext } from 'context/checkedStatesContext'
 import States from 'resources/statesData'
 import * as S from './states.styles'
 
-const StateCheckboxList = () => {
+const StateCheckboxList = ({ statesData }) => {
+  const [visible, setVisible] = useState(7)
+  // eslint-disable-next-line no-unused-vars
+  const [_, setStatesList] = useState(statesData)
+  const { handleCheckBoxFunction } = useContext(CheckedStatesContext)
+
+  useEffect(() => {
+    const stateSelected = statesData.map((member) => {
+      return member
+    })
+    setStatesList(stateSelected)
+    console.log(stateSelected)
+  }, [statesData])
+
+  const showMoreStates = (event) => {
+    event.preventDefault()
+    if (visible === 7) {
+      setVisible((vis) => vis + 20)
+    } else {
+      setVisible((vis) => vis - 20)
+    }
+  }
   return (
     <S.List>
       <span className="title">Por Estado</span>
-      {States.map((state) => {
+      {States.slice(0, visible).map((state) => {
+        const lowerState = state.toLowerCase()
         return (
           <S.Wrapper key={state}>
             <ul>
               <li>
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  value={lowerState}
+                  onClick={handleCheckBoxFunction}
+                />
                 {state}
               </li>
             </ul>
           </S.Wrapper>
         )
       })}
-      <a href="#">Ver mais</a>
+      <a href="#" onClick={showMoreStates}>
+        {visible !== 27 ? 'Ver mais' : 'Ver menos'}
+      </a>
     </S.List>
   )
 }
