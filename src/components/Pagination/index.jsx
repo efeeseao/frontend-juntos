33d56f2518bf7dcdfdedc0c7b4e931ac75d39a52
Fernando Santos <nando4803@gmail.com>
2021-09-19 23:19:30 +0100

    fix: pagination

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,26 +1,162 @@
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import { useState, useEffect } from 'react'
+
+import range from 'components/Shared/rage'
+import leftArrow from 'images/left-path.png'
+import rightArrow from 'images/right-path.png'
 import * as S from './pagination.styles'
 
-const Pagination = () => {
+const LEFT_PAGE = 'LEFT'
+const RIGHT_PAGE = 'RIGHT'
+
+const Pagination = (props) => {
+  const [isCurrentPage, setIsCurrentPage] = useState(1)
+  let { totalMembers, pageLimit, pageNeighbors } = props
+
+  totalMembers = typeof totalUsers === 'number' ? totalMembers : 200
+  pageLimit = typeof pageLimit === 'number' ? pageLimit : 9
+
+  pageNeighbors =
+    typeof pageNeighbors === 'number'
+      ? Math.max(0, Math.min(pageNeighbors, 2))
+      : 0
+
+  const totalPages = Math.ceil(totalMembers / pageLimit)
+
+  const gotoPage = (page) => {
+    const { onPageChanged = (i) => i } = props
+
+    const currentPage = Math.max(0, Math.min(page, totalPages))
+
+    const paginationData = {
+      currentPage,
+      totalPages,
+      pageLimit,
+      totalMembers
+    }
+    setIsCurrentPage(currentPage)
+    onPageChanged(paginationData)
+  }
+
+  useEffect(() => {
+    gotoPage(1)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleClick = (page, evt) => {
+    evt.preventDefault()
+    gotoPage(page)
+  }
+
+  const handleMoveLeft = (evt) => {
+    evt.preventDefault()
+    gotoPage(isCurrentPage - pageNeighbors * 2 - 1)
+  }
+
+  const handleMoveRight = (evt) => {
+    evt.preventDefault()
+    gotoPage(isCurrentPage + pageNeighbors * 2 + 1)
+  }
+
+  const fetchPageNumbers = () => {
+    const totalNumbers = pageNeighbors * 2 + 3
+    const totalBlocks = totalNumbers + 2
+
+    if (totalPages > totalBlocks) {
+      let pages = []
+
+      const leftBound = isCurrentPage - pageNeighbors
+      const rightBound = isCurrentPage + pageNeighbors
+      const beforeLastPage = totalPages - 1
+
+      const startPage = leftBound > 2 ? leftBound : 2
+      const endPage = rightBound < beforeLastPage ? rightBound : beforeLastPage
+
+      pages = range(startPage, endPage)
+
+      const pagesCount = pages.length
+      const singleSpillOffset = totalNumbers - pagesCount - 1
+
+      const leftSpill = startPage > 2
+      const rightSpill = endPage < beforeLastPage
+
+      const leftSpillPage = LEFT_PAGE
+      const rightSpillPage = RIGHT_PAGE
+
+      if (leftSpill && !rightSpill) {
+        const extraPages = range(startPage - singleSpillOffset, startPage - 1)
+        pages = [leftSpillPage, ...extraPages, ...pages]
+      } else if (!leftSpill && rightSpill) {
+        const extraPages = range(endPage + 1, endPage + singleSpillOffset)
+        pages = [...pages, ...extraPages, rightSpillPage]
+      } else if (leftSpill && rightSpill) {
+        pages = [leftSpillPage, ...pages, rightSpillPage]
+      }
+
+      return [1, ...pages, totalPages]
+    }
+
+    return range(1, totalPages)
+  }
+
+  if (!totalMembers) return null
+
+  if (totalPages === 1) return null
+
+  const pages = fetchPageNumbers()
+
   return (
     <S.PaginationWrapper>
-      <button role="button" type="button" className="left-button">
-        <FaArrowLeft />
+      <button
+        type="button"
+        className="left-button"
+        disabled={isCurrentPage === 1}
+        onClick={(event) => handleClick(isCurrentPage - 1, event)}
+      >
+        <img src={leftArrow} alt="button-icon" />
       </button>
       <ul className="pagination">
-        <li className="page-item">
-          <a href="#" className="page-link">
-            <span>&laquo;</span>
-          </a>
-        </li>
-        <li className="page-item">
-          <a href="#" className="page-link">
-            12
-          </a>
-        </li>
+        {pages.map((page, index) => {
+          if (page === LEFT_PAGE)
+            return (
+              <li key={index} className="page-item">
+                <a className="page-link" href="#" onClick={handleMoveLeft}>
+                  <span>&laquo;</span>
+                </a>
+              </li>
+            )
+
+          if (page === RIGHT_PAGE)
+            return (
+              <li key={index} className="page-item">
+                <a className="page-link" href="#" onClick={handleMoveRight}>
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            )
+
+          return (
+            <li
+              key={index}
+              className={`page-item${isCurrentPage === page ? ' active' : ''}`}
+            >
+              <a
+                className="page-link"
+                href="#"
+                onClick={(event) => handleClick(page, event)}
+              >
+                {page}
+              </a>
+            </li>
+          )
+        })}
       </ul>
-      <button role="button" type="button" className="right-button">
-        <FaArrowRight />
+      <button
+        type="button"
+        className="right-button"
+        disabled={isCurrentPage === 23}
+        onClick={(event) => handleClick(isCurrentPage + 1, event)}
+      >
+        <img src={rightArrow} alt="button-icon" />
       </button>
     </S.PaginationWrapper>
   )
--- src/components/Pagination/pagination.styles.js
+++ src/components/Pagination/pagination.styles.js
@@ -1,64 +1,58 @@
 import styled from 'styled-components'
 
 export const PaginationWrapper = styled.nav`
-  align-items: center;
   display: flex;
-  justify-content: space-between;
   height: 40px;
-  margin-bottom: 80px;
   width: 350px;
+  margin-bottom: 80px;
+  align-items: space-between;
+  align-items: center;
+
+  .left-button,
+  .right-button {
+    cursor: pointer;
+    border: none;
+    width: 52px;
+    height: 42px;
+    margin: auto;
+    border-radius: 50%;
+    background-color: #4a4a4a;
+    text-align: center;
+  }
+
+  .left-button:disabled,
+  .right-button:disabled {
+    background-color: #9b9b9b;
+  }
 
   .pagination {
-    align-items: center;
     display: flex;
+    align-items: center;
     justify-content: center;
     list-style: none;
     padding: 0;
-    text-align: center;
     width: 100%;
+    text-align: center;
 
     .page-item {
       display: inline;
       margin: 0 12px;
-      padding-bottom: 8px;
 
       .page-link {
-        color: #9b9b9b;
-        font-size: 1.5rem;
+        font-size: 16px;
         height: 40px;
+        color: #9b9b9b;
         text-decoration: none;
       }
     }
 
     .page-item.active {
-      border-bottom: 3px solid #9b9b9b;
-      border-radius: 3px;
+      margin: 0;
       padding: 0 16px 5px;
     }
 
     .page-item.active a {
       font-weight: 700;
     }
-  }
-
-  .left-button,
-  .right-button {
-    background-color: #4a4a4a;
-    border: none;
-    border-radius: 50%;
-    cursor: pointer;
-    height: 44px;
-    margin: auto;
-    text-align: center;
-    width: 60px;
-
-    svg {
-      color: #ffffff;
-    }
-  }
-
-  .left-button:disabled,
-  .right-button:disabled {
-    background-color: #9b9b9b;
   }
 `
